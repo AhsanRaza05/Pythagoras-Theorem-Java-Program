18ab9b268b9db2eccc5cca364944c2ffe355d0e7
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-05-27 10:23:56 +0500

    Give All Possible Pythagoras Triplets for given value of Hypoteneous

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 
 
 import java.util.Scanner;
+import static java.lang.System.out;
 
 class Main{
 
@@ -9,23 +10,32 @@ class Main{
 		Scanner input = new Scanner(System.in);
 		int hypoteneous = 0;
 		
-		System.out.print("Please enter the value of Hypoteneous: ");
+		System.out.print("\nPlease enter the value of Hypoteneous: ");
 		hypoteneous = input.nextInt();
 		
-		l1:for(int base = 0; base < hypoteneous;base++){
+		out.println();
+
+		int totalPossibleTriplets = 0;
+
+		l1:for(int perpendicular = 0; perpendicular < hypoteneous; perpendicular++){
 			
-		
-			for(int perpendicular = 0; perpendicular < hypoteneous; perpendicular++){
-				
+			for(int base = 0; base < perpendicular;base++){	
 								
 				if(Math.pow(hypoteneous,2) == (Math.pow(base,2) + Math.pow(perpendicular,2))){
 				
-				System.out.println("The Value of Base = %d\nThe value of Perpendicular = %d".formatted(base,perpendicular));
-				break l1;
+				totalPossibleTriplets++;
+				
+				out.println("For TRIPLET # %d".formatted(totalPossibleTriplets));
+
+				out.println("\tThe Value of Base = %d\n\tThe value of Perpendicular = %d\n".formatted(base,perpendicular));
+
+				//break l1;
 			}
 			}
 			
 		}
+
+		out.print("Total no. of Possible triplets = %d\n".formatted(totalPossibleTriplets));
 		
 		
 	}
